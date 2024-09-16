676fe7ebfcd1cea9deef0b8f8f22c9848968cd30
Riku <riku.block@gmail.com>
2024-09-16 09:52:24 +0200

    fix: static site urls

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,7 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: "https://rikublock.github.io/metadata-reader/",
+  },
 });
