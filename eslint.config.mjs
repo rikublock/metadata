5681d6d314adeb09ba02070fbd18991d3bd640b5
Riku <riku.block@gmail.com>
2024-09-18 18:28:24 +0200

    update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,8 @@ export default [
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
   { ignores: ["dist/"] },
